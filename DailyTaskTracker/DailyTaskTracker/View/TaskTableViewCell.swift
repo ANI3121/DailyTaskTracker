205b2fad6daf12808e1e618dbea66a1c30dd4490
Anirudha Kumar <ani.pat007@gmail.com>
2021-03-11 20:35:30 +0530

    UI Improvement

--- DailyTaskTracker/DailyTaskTracker/View/TaskTableViewCell.swift
+++ DailyTaskTracker/DailyTaskTracker/View/TaskTableViewCell.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class TaskTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var endTimeLabel: UILabel!
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var startTimeLabel: UILabel!
@@ -20,6 +21,10 @@ class TaskTableViewCell: UITableViewCell {
     }
     
     func configureCellView() {
+        containerView.layer.cornerRadius = 10
+        containerView.clipsToBounds = true
+        containerView.layer.shadowRadius = 10
+        containerView.layer.borderWidth = 1.0
         statusLabel.layer.cornerRadius = 5.0
         statusLabel.layer.borderWidth = 0.5
         statusLabel.clipsToBounds = true
